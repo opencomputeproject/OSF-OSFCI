d8506ac7c808c6d4f794934afda5844051d64af2
vejmarie <jean-marie.verdun@hpe.com>
2020-12-17 13:13:45 -0500

    Add missing piece to previous commit
    Signed-off-by: vejmarie <jean-marie.verdun@hpe.com>

--- gateway/server.go
+++ gateway/server.go
@@ -407,7 +407,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 			if ( cacheIndex != -1 ) {
                                 url, _ := url.Parse("http://"+ciServers.servers[cacheIndex].ip+ciServers.servers[cacheIndex].tcpPort)
                                 proxy := httputil.NewSingleHostReverseProxy(url)
-                                r.URL.Path = "/resetEmulator" + tail
+                                r.URL.Path = tail
 				fmt.Printf(r.URL.Path)
                                 r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
                                 proxy.ServeHTTP(w , r)
