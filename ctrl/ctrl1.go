6eaaea7db9eef3904eb22b53e7777e49562d4f74
Arun Koshy <arunkoshy@gmail.com>
2021-03-29 11:18:55 -0700

    misspell
    typo fix

--- ctrl/ctrl1.go
+++ ctrl/ctrl1.go
@@ -260,7 +260,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		// We must get the username from the request
 		_, tail := ShiftPath(r.URL.Path)
 		login := tail[1:]
-		// We have to retreive the BIOS from the compile server
+		// We have to retrieve the BIOS from the compile server
 
 		_ = base.HTTPGetRequest("http://" + compileUri + compileTcpPort + "/cleanUp/rom")
 		myfirmware := base.HTTPGetRequest("http://" + storageUri + storageTcpPort + "/user/" + login + "/getFirmware")
@@ -317,7 +317,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		// We must get the username from the request
 		_, tail := ShiftPath(r.URL.Path)
 		login := tail[1:]
-		// We have to retreive the BIOS from the storage server
+		// We have to retrieve the BIOS from the storage server
 
 		_ = base.HTTPGetRequest("http://" + compileUri + compileTcpPort + "/cleanUp/bmc")
 		myfirmware := base.HTTPGetRequest("http://" + storageUri + storageTcpPort + "/user/" + login + "/getBMCFirmware")
