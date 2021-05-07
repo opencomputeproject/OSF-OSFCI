d7fbec46d067e1dd394e2130ba2fc76deab8f64e
Arun Koshy <arunkoshy@gmail.com>
2021-05-06 18:05:52 -0700

    store log in json

--- gateway/server.go
+++ gateway/server.go
@@ -841,7 +841,7 @@ func init() {
 		ConsoleJSONFormat: false,                                   //Console log in JSON format, false will print in raw format on console
 		EnableFile:        true,                                    // Logging in File
 		FileLevel:         base.Info,                               // File log level
-		FileJSONFormat:    false,                                   // File JSON Format, False will print in file in raw Format
+		FileJSONFormat:    true,                                    // File JSON Format, False will print in file in raw Format
 		FileLocation:      "/usr/local/production/logs/server.log", //File location where log needs to be appended
 	}
 
