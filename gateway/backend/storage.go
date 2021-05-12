0b0991c9340a8b54512c7f0d84413fedd6b1fc0a
Arun Koshy <arunkoshy@gmail.com>
2021-05-12 14:20:33 -0700

    log toggle for json

--- gateway/backend/storage.go
+++ gateway/backend/storage.go
@@ -285,7 +285,7 @@ func init() {
 		ConsoleJSONFormat: false,                                    //Console log in JSON format, false will print in raw format on console
 		EnableFile:        true,                                     // Logging in File
 		FileLevel:         base.Info,                                // File log level
-		FileJSONFormat:    false,                                    // File JSON Format, False will print in file in raw Format
+		FileJSONFormat:    true,                                     // File JSON Format, False will print in file in raw Format
 		FileLocation:      "/usr/local/production/logs/storage.log", //File location where log needs to be appended
 	}
 
